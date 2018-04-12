8b06b4220f274d6ec45d2f9b4df9a482b2148a4f
SHAAN CHAUDHARI <S199892341@OCDSB>
2018-04-12 11:04:24 -0400

    Spotify Logo

--- logo.py
+++ logo.py
@@ -1,11 +1,12 @@
 # ICS3U
 # Assignment 2: Logo
-# <your name>
+# <Shaan>
 
 # adapted from http://www.101computing.net/getting-started-with-pygame/
 
 # Import the pygame library and initialise the game engine
 import pygame
+import math
 pygame.init()
 
 # Define some colours
@@ -14,7 +15,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-
+LGREEN = (86, 219, 74)
 # Set the screen size (please don't change this)
 SCREENWIDTH = 400
 SCREENHEIGHT = 400
@@ -23,7 +24,7 @@ SCREENHEIGHT = 400
 # The window is defined as (width, height), measured in pixels
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Logo")
+pygame.display.set_caption("Shanns Logo")
 
 # This loop will continue until the user exits the game
 carryOn = True
@@ -44,13 +45,14 @@ while carryOn:
     # --- Draw code goes here
 
     # Clear the screen to white
-    screen.fill(WHITE)
+    screen.fill(BLACK)
 
     # Queue different shapes and lines to be drawn
     # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
     # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
-
+    pygame.draw.ellipse(screen, LGREEN, [50,50, 300, 300], 0)
+    pygame.draw.ellipse(screen, WHITE, [95,130, 25, 10], 0)
+    pygame.draw.arc(screen, WHITE, [100, 100, 200, 80], math.radians(0), math.radians(180), 20)
     # Update the screen with queued shapes
     pygame.display.flip()
 
